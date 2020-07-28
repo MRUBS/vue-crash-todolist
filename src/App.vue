5622550572f597c73f812fc2550c9e7c47175468
M Rub Shuvo <MRUBS@users.noreply.github.com>
2020-07-28 14:28:34 +0600

    get ready vue with components

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos.vue";
+
+export default {
+  name: "App",
+  components: {
+    Todos,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: false,
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: true,
+        },
+        {
+          id: 4,
+          title: "Todo Four",
+          completed: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" name="todoComplete" id="todoComplete" v-on:change="markComplete" />
+      <label for="todoComplete">{{todo.title}}</label>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      console.log(123);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border: 1px #ccc dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>
